58fd0c92daa9e4be9c373811ccfc39e6270dbf4e
Your Name <you@example.com>
2024-09-07 11:25:50 +0700

    start project

---
+++ application/controllers/todoController.php
@@ -0,0 +1,5 @@
+<?php
+
+class todoCotroller extends CI_Controller{
+    
+}
---
+++ application/models/todomodel.php
@@ -0,0 +1,23 @@
+<?php
+
+class todomodel extends CI_Model{
+
+    public function todoAkun($id){
+        $this->db->where('id_akun',$id);
+        return $this->db->get('todo');
+    }
+
+    public function insertTodo($data){
+        return $this->db->insert('todo',$data);
+    }
+
+    public function updateTodo($id,$data){
+        $this->db->where('id_akun',$id);
+        return $this->db->update('todo',$data);
+    }
+
+    public function deleteTodo($id){
+        $this->db->where('id_akun',$id);
+        return $this->db->delete('todo');
+    }
+}
